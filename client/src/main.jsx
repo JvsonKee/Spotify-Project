8bd2096084ea39813945ce3bfa1b3c109d71d5e9
JvsonKee <jasonk2723@gmail.com>
2024-02-20 15:11:11 -0700

    routing fixes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,10 +14,6 @@ const router = createBrowserRouter([
     element: <App />,
   },
   {
-    path: 'profile',
-    element: <Profile />
-  },
-  {
     path: 'playlists',
     element: <Playlists />
   },
